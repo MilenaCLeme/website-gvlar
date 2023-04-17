33f5fcd85f51a85dd081bd12de00e6ea066d2caa
MilenaCLeme <milenaleme4@hotmail.com>
2023-04-17 13:54:35 -0300

    feas: insert prisma module and function prisma service user

--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+
+  async listUsers(): Promise<User[]> {
+    return await this.prisma.user.findMany();
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return await this.prisma.user.create({
+      data,
+    });
+  }
+}
