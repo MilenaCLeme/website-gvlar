6dcb93731ca9c6839963b4c4a1b63cef13396b4f
MilenaCLeme <milenaleme4@hotmail.com>
2023-04-17 20:46:46 -0300

    feas: delet service and controller init

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   exports: [],
 })
 export class AppModule {}
