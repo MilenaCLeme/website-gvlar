e8fb305442bb3c2a7a936115cd1462e152d54732
Milena Leme <milenaleme4@hotmail.com>
2023-05-11 00:21:15 -0300

    feas: insert guard auth for token and role

---
+++ back-end/src/guards/auth.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest().headers;
+    const { authorization } = request;
+
+    try {
+      const data = this.authService.checkToken(
+        (authorization ?? '').split(' ')[1],
+      );
+
+      request.tokenPayload = data;
+
+      request.user = await this.userService.showId(data.id);
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
