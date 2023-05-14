ce7e249321daf768a0cd3f8c3c145f1e9e0da95b
Milena Leme <milenaleme4@hotmail.com>
2023-05-13 22:56:29 -0300

    feas: insert headers and .user

--- back-end/src/decorators/user.decorator.ts
+++ back-end/src/decorators/user.decorator.ts
@@ -6,11 +6,11 @@ import {
 
 export const User = createParamDecorator(
   (filter: string, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest().headers;
 
     if (request.user) {
       if (filter) {
-        return request[filter];
+        return request.user[filter];
       } else {
         return request.user;
       }
