32c16ab37eeb2671acaeb0dadb5988ac5fa9bfb6
Milena Leme <milenaleme4@hotmail.com>
2023-05-22 20:04:28 -0300

    feas: insert module for immobile

--- back-end/src/immobile/immobile.module.ts
+++ back-end/src/immobile/immobile.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ImmobileController } from './immobile.controller';
 import { ImmobileService } from './immobile.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    forwardRef(() => PrismaModule),
+    forwardRef(() => AuthModule),
+    forwardRef(() => UserModule),
+  ],
   controllers: [ImmobileController],
   providers: [ImmobileService],
-  exports: [],
+  exports: [ImmobileService],
 })
 export class ImmobileModule {}
