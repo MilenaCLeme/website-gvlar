a73c7ea6e8795100d715780eed99b6b7d5472eb9
Milena Leme <milenaleme4@hotmail.com>
2023-05-24 17:18:25 -0300

    feaS: insert useguards authGuard and RoleGuard and use Interceptors for info time

--- back-end/src/immobile/immobile.controller.ts
+++ back-end/src/immobile/immobile.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
   Put,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ImmobileService } from './immobile.service';
 import { ParamId } from 'src/decorators/param-id.decorator';
@@ -14,38 +15,54 @@ import { CreateImmobileDTO } from './dto/create-immobile.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/decorators/user.decorator';
 import { User as UserType } from '@prisma/client';
+import { UpdatePutImmobileDTO } from './dto/update-put-user.dto';
+import { UpdatePatchImmobileDTO } from './dto/update-patch-immobile.dto';
+import { RoleGuard } from 'src/guards/role.guard';
+import { LogInterceptor } from 'src/interceptors/log.interceptor';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
 
-@UseGuards(AuthGuard)
-@Controller('immobile')
+@UseGuards(AuthGuard, RoleGuard)
+@UseInterceptors(LogInterceptor)
+@Controller('immobiles')
 export class ImmobileController {
   constructor(private readonly immobileService: ImmobileService) {}
 
+  @Roles(Role.master, Role.worker)
   @Post()
   async create(@Body() data: CreateImmobileDTO, @User() user: UserType) {
     return await this.immobileService.create(data, user);
   }
 
+  @Roles(Role.master)
   @Get()
   async list() {
     return await this.immobileService.immobiles();
   }
 
+  @Roles(Role.master, Role.worker)
   @Get(':id')
   async showId(@ParamId() id: number) {
     return await this.immobileService.showId(id);
   }
 
-  @Put('id')
-  async update(@ParamId() id: number) {
-    return await this.immobileService.update(id);
+  @Roles(Role.master, Role.worker)
+  @Put(':id')
+  async update(@ParamId() id: number, @Body() data: UpdatePutImmobileDTO) {
+    return await this.immobileService.update(id, data);
   }
 
-  @Patch('id')
-  async updatePartial(@ParamId() id: number) {
-    return await this.immobileService.update(id);
+  @Roles(Role.master, Role.worker)
+  @Patch(':id')
+  async updatePartial(
+    @ParamId() id: number,
+    @Body() data: UpdatePatchImmobileDTO,
+  ) {
+    return await this.immobileService.update(id, data);
   }
 
-  @Delete('id')
+  @Roles(Role.master)
+  @Delete(':id')
   async delete(@ParamId() id: number) {
     return await this.immobileService.delete(id);
   }
