4f3c9afe4ac53fcd34fbe467bdb7ec371e82fdaf
Milena Leme <milenaleme4@hotmail.com>
2023-05-29 22:49:37 -0300

    feas: insert immobileModule and ImmobileOnOwnerModule with ownerModule

--- back-end/src/owner/owner.module.ts
+++ back-end/src/owner/owner.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { OwnerController } from './owner.controller';
 import { OwnerService } from './owner.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ImmobileModule } from 'src/immobile/immobile.module';
+import { ImmobileOnOwnerModule } from 'src/immobileOnOwner/immobileonowner.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    forwardRef(() => PrismaModule),
+    forwardRef(() => ImmobileModule),
+    forwardRef(() => ImmobileOnOwnerModule),
+  ],
   controllers: [OwnerController],
   providers: [OwnerService],
   exports: [OwnerService],
