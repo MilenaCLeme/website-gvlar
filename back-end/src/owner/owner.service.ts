097dea0cd64b66fe612b6f0131d82deac833c11d
Milena Leme <milenaleme4@hotmail.com>
2023-05-29 22:52:00 -0300

    feas: insert news functions

--- back-end/src/owner/owner.service.ts
+++ back-end/src/owner/owner.service.ts
@@ -1,50 +1,137 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  forwardRef,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateOwnerDTO } from './dto/create-owner.dto';
+import { Owner, Prisma } from '@prisma/client';
+import { ImmobileService } from 'src/immobile/immobile.service';
+import { UpdatePatchOwnerDTO } from './dto/update-patch-owner.dto';
+import { ImmobileOnOwnerService } from 'src/immobileOnOwner/immobileonowner.service';
 
 @Injectable()
 export class OwnerService {
-  constructor(private readonly prisma: PrismaService) {}
-  async list() {
-    return [];
-  }
+  constructor(
+    @Inject(forwardRef(() => PrismaService))
+    private readonly prisma: PrismaService,
+    @Inject(forwardRef(() => ImmobileService))
+    private readonly immobileService: ImmobileService,
+    @Inject(forwardRef(() => ImmobileOnOwnerService))
+    private readonly immobileOnOwner: ImmobileOnOwnerService,
+  ) {}
 
-  async showId(id: number) {
-    return id;
-  }
-
-  async create(createOwner: CreateOwnerDTO) {
-    return await this.prisma.owner.create({
-      data: {
-        name: createOwner.name,
-        email: createOwner.email,
-        phone: createOwner.phone,
-        immobiles: {
-          create: {
-            immobile: {
-              connect: {
-                id: createOwner.idImmobile,
-              },
-            },
-          },
-        },
-      },
+  async owner(
+    userWhereUniqueInput: Prisma.OwnerWhereUniqueInput,
+  ): Promise<Owner | null> {
+    return await this.prisma.owner.findUnique({
+      where: userWhereUniqueInput,
       include: {
         immobiles: {
           include: {
             immobile: true,
-            owner: true,
           },
         },
       },
     });
   }
 
-  async update(id: number, data) {
-    return { id, data };
+  async list() {
+    return await this.prisma.owner.findMany({
+      include: {
+        immobiles: {
+          include: {
+            immobile: true,
+          },
+        },
+      },
+    });
+  }
+
+  async showId(id: number) {
+    await this.exists(id);
+    return await this.owner({ id });
+  }
+
+  async create(create: CreateOwnerDTO) {
+    try {
+      await this.immobileService.exists(create.idImmobile);
+      return await this.prisma.owner.create({
+        data: {
+          name: create.name,
+          email: create.email,
+          phone: create.phone,
+          immobiles: {
+            create: {
+              immobile: {
+                connect: {
+                  id: create.idImmobile,
+                },
+              },
+            },
+          },
+        },
+        include: {
+          immobiles: {
+            include: {
+              immobile: true,
+              owner: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException(
+          'O e-mail já está em uso. Por favor, pesquisar o e-mail para localizar usuario.',
+        );
+      }
+      throw new InternalServerErrorException(
+        'Ocorreu um erro inesperado ao cadastrar',
+      );
+    }
+  }
+
+  async update(id: number, update: UpdatePatchOwnerDTO) {
+    const { email, name, phone } = update;
+    await this.exists(id);
+    return await this.prisma.owner.update({
+      where: {
+        id,
+      },
+      data: {
+        ...(name && { name }),
+        ...(email && { email }),
+        ...(phone && { phone }),
+      },
+    });
   }
 
   async detele(id: number) {
-    return { id };
+    await this.exists(id);
+    await this.immobileOnOwner.deleteOwner(id);
+    return await this.prisma.owner.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async exists(id: number) {
+    if (
+      !(await this.prisma.owner.count({
+        where: {
+          id,
+        },
+      }))
+    ) {
+      throw new NotFoundException(`O ${id} não existe`);
+    }
   }
 }
