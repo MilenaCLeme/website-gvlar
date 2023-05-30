3829630b2aa766b81215398675ad18a9dd7e2462
Milena Leme <milenaleme4@hotmail.com>
2023-05-29 22:03:10 -0300

    feas: insert immobileonownermodule in immobilemodule

--- back-end/src/immobile/immobile.module.ts
+++ back-end/src/immobile/immobile.module.ts
@@ -4,12 +4,14 @@ import { ImmobileService } from './immobile.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
+import { ImmobileOnOwnerModule } from 'src/immobileOnOwner/immobileonowner.module';
 
 @Module({
   imports: [
     forwardRef(() => PrismaModule),
     forwardRef(() => AuthModule),
     forwardRef(() => UserModule),
+    forwardRef(() => ImmobileOnOwnerModule),
   ],
   controllers: [ImmobileController],
   providers: [ImmobileService],
