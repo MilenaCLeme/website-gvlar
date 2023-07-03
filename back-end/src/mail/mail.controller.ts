eb65c4ae019c62e0b0bd78c1fadeae7eca3fe9c4
Milena Leme <milenaleme4@hotmail.com>
2023-07-03 12:12:05 -0300

    feas: insert logInterceptor

--- back-end/src/mail/mail.controller.ts
+++ back-end/src/mail/mail.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { MailSendDTO } from './dto/mail-send.dto';
 import { MailService } from './mail.service';
+import { LogInterceptor } from 'src/interceptors/log.interceptor';
 
+@UseInterceptors(LogInterceptor)
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
