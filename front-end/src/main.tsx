f8dc8c27ed12df19db2d3b1949ead67bcfcbf30f
Milena Leme <milenaleme4@hotmail.com>
2023-07-26 14:51:48 -0300

    feat: insert ContextProvider and BrowserRouter

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ContextProvider } from './context/Provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
   </React.StrictMode>,
 );
