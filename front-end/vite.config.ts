61a40d72b0a7dabfa7723758eb5a0a441a3f7d8c
Milena Leme <milenaleme4@hotmail.com>
2023-07-26 14:52:54 -0300

    feat: modified vite

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
@@ -9,5 +10,18 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react()],
+  plugins: [
+    svgr({
+      exportAsDefault: true,
+      include: '**/*.svg',
+    }),
+    react(),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '../src/style/variables.scss';`,
+      },
+    },
+  },
 });
