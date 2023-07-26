feccc449d6d5a92a0ab0d75c63f153ff9ed2f397
Milena Leme <milenaleme4@hotmail.com>
2023-07-26 14:47:15 -0300

    feat: insert news routes

--- front-end/src/router/index.tsx
+++ front-end/src/router/index.tsx
@@ -1,7 +1,27 @@
-import Home from '@/page/home/Home';
+import DefaulPageADM from '@/components/DefaulPageADM';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Filter from '@/page/Filter';
+import Home from '@/page/Home';
+import LoginAndRegister from '@/page/LoginAndRegister';
+import MyData from '@/page/MyData';
+import { Route, Routes } from 'react-router-dom';
 
 const Router = () => {
-  return <Home />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/encontrar' element={<Filter />} />
+        <Route path='/login' element={<LoginAndRegister />} />
+        <Route path='/adm' element={<DefaulPageADM />}>
+          <Route index element={<MyData />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 
 export default Router;
