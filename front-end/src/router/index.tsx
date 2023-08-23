c828153fe2bdf736d086839de3d14e636deebe3d
Milena Leme <milenaleme4@hotmail.com>
2023-08-23 09:51:51 -0300

    feat: modified insert return

--- front-end/src/router/index.tsx
+++ front-end/src/router/index.tsx
@@ -3,13 +3,15 @@ import LoginAndRegister from '@/page/LoginAndRegister';
 import { Route, Routes } from 'react-router-dom';
 
 const Router = () => {
-  <main className='container'>
-    <Routes>
-      <Route path='/' element={<DefaulPage />}>
-        <Route path='login' element={<LoginAndRegister />} />
-      </Route>
-    </Routes>
-  </main>;
+  return (
+    <main className='container'>
+      <Routes>
+        <Route path='/' element={<DefaulPage />}>
+          <Route path='login' element={<LoginAndRegister />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default Router;
