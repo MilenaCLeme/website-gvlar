ee36d4022b4a96718aea8b5cbd76e9c8e9b030f7
Milena Leme <milenaleme4@hotmail.com>
2023-08-23 09:39:52 -0300

    feat: insert router and Default page

---
+++ front-end/src/router/index.tsx
@@ -0,0 +1,15 @@
+import DefaulPage from '@/components/DefaulPage';
+import LoginAndRegister from '@/page/LoginAndRegister';
+import { Route, Routes } from 'react-router-dom';
+
+const Router = () => {
+  <main className='container'>
+    <Routes>
+      <Route path='/' element={<DefaulPage />}>
+        <Route path='login' element={<LoginAndRegister />} />
+      </Route>
+    </Routes>
+  </main>;
+};
+
+export default Router;
