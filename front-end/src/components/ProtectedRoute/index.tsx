cdf644fa277215ece74852e3f5e0ddadc65865e4
Milena Leme <milenaleme4@hotmail.com>
2023-08-31 16:22:04 -0300

    feat: protected ROuter

---
+++ front-end/src/components/ProtectedRoute/index.tsx
@@ -0,0 +1,20 @@
+import { Context } from '@/context';
+import { ReactNode, useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }: ProtectedRouteProps) => {
+  const { token } = useContext(Context);
+  const { pathname } = useLocation();
+
+  if (token === '' && pathname !== '/login') {
+    return <Navigate to='/login' replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
