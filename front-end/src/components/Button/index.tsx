8cc7d30c7461e7c47bbed1094cf8af8cde827aea
Milena Leme <milenaleme4@hotmail.com>
2023-09-27 20:57:22 -0300

    feat: modified Button

--- front-end/src/components/Button/index.tsx
+++ front-end/src/components/Button/index.tsx
@@ -1,13 +1,14 @@
-interface ButtonProps {
-  name: string;
-  onClick: () => void;
-  className?: string;
-  disabled?: boolean;
-}
+import style from './button.module.scss';
+import { Button as ButtonProps } from '@/types';
 
 const Button: React.FC<ButtonProps> = ({ name, onClick, className, disabled }: ButtonProps) => {
   return (
-    <button type='button' disabled={disabled} onClick={onClick} className={className}>
+    <button
+      type='button'
+      disabled={disabled}
+      onClick={onClick}
+      className={className ? className : style.button}
+    >
       {name}
     </button>
   );
