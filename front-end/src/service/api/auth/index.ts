1e93bb4d8f25ea8941d328a265740408b700e0ce
Milena Leme <milenaleme4@hotmail.com>
2023-09-27 21:04:01 -0300

    feat: insert news functions

--- front-end/src/service/api/auth/index.ts
+++ front-end/src/service/api/auth/index.ts
@@ -1,6 +1,28 @@
 import api from '../axios-config';
-import { CreateUserClient, ErrorAxios, GetUser, Login, Sucess } from '@/types';
+import { CreateUserClient, ErrorAxios, GetUser, Login, Sucess, UpdateUser, User } from '@/types';
 
+interface ChangePassword {
+  passwordOld: string;
+  passwordNew: string;
+}
+
+// Token
+export const checkToken = async (token: string) => {
+  try {
+    const { data } = await api.get<GetUser>('/auth/check-token', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return data as GetUser;
+  } catch (error: any) {
+    if ('message' in error) {
+      return error as ErrorAxios;
+    } else {
+      console.log('Erro inesperado:', error);
+    }
+  }
+};
+
+// User
 export const loginUser = async (body: Login) => {
   try {
     const { data } = await api.post<GetUser>('/auth/login', {
@@ -18,7 +40,7 @@ export const loginUser = async (body: Login) => {
 
 export const registerUser = async (body: CreateUserClient) => {
   try {
-    const { data } = await api.post('/auth/register', { ...body });
+    const { data } = await api.post<GetUser>('/auth/register', { ...body });
 
     return data as GetUser;
   } catch (error: any) {
@@ -32,9 +54,65 @@ export const registerUser = async (body: CreateUserClient) => {
 
 export const resendEmail = async (email: string) => {
   try {
-    const { data } = await api.post('/auth/validate', { email });
+    const { data } = await api.post<Sucess>('/auth/validate', { email });
 
     return data as Sucess;
+  } catch (error: any) {
+    if ('message' in error) {
+      return error as ErrorAxios;
+    } else {
+      console.log('Erro inesperado:', error);
+    }
+  }
+};
+
+export const logout = async (token: string) => {
+  try {
+    const { data } = await api.get<Sucess>('/auth/logout', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return data as Sucess;
+  } catch (error: any) {
+    if ('message' in error) {
+      return error as ErrorAxios;
+    } else {
+      console.log('Erro inesperado:', error);
+    }
+  }
+};
+
+export const updateUser = async (token: string, body: UpdateUser) => {
+  try {
+    const { data } = await api.patch<User>(
+      '/auth/register',
+      { ...body },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    );
+
+    return data as User;
+  } catch (error: any) {
+    if ('message' in error) {
+      return error as ErrorAxios;
+    } else {
+      console.log('Erro inesperado:', error);
+    }
+  }
+};
+
+export const changePassword = async (token: string, body: ChangePassword) => {
+  try {
+    const { data } = await api.patch<User>(
+      '/auth/changepassword',
+      { ...body },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    );
+
+    return data as User;
   } catch (error: any) {
     if ('message' in error) {
       return error as ErrorAxios;
