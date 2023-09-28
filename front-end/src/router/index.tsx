8bab60f74add6748ea7c181730f049cbf0708433
Milena Leme <milenaleme4@hotmail.com>
2023-09-27 21:01:48 -0300

    feat: modified router

--- front-end/src/router/index.tsx
+++ front-end/src/router/index.tsx
@@ -1,5 +1,9 @@
 import DefaulPage from '@/components/DefaulPage';
+import DefaulPageADM from '@/components/DefaulPageADM';
 import ProtectedRoute from '@/components/ProtectedRoute';
+import ChangePassword from '@/page/ADM/ChangePassword';
+import MyData from '@/page/ADM/MyData';
+import MyProperty from '@/page/ADM/MyProperty';
 import LoginAndRegister from '@/page/LoginAndRegister';
 import { Route, Routes } from 'react-router-dom';
 
@@ -16,6 +20,32 @@ const Router = () => {
               </ProtectedRoute>
             }
           />
+          <Route path='adm' element={<DefaulPageADM />}>
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <MyData />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='senha'
+              element={
+                <ProtectedRoute>
+                  <ChangePassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='imoveis'
+              element={
+                <ProtectedRoute>
+                  <MyProperty />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
         </Route>
       </Routes>
     </main>
