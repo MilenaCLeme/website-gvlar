4dd62fd978b297174345db02c0698a225f8538ae
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 15:21:45 -0300

    feat: insert useCallBack, start and fetchData

--- front-end/src/service/hook/use-axios/index.ts
+++ front-end/src/service/hook/use-axios/index.ts
@@ -1,6 +1,6 @@
 import { ErrorAxios } from '@/types';
 import { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 interface ConfigRequest {
   axiosInstance: AxiosInstance;
@@ -14,32 +14,38 @@ const useAxios = (configRequest: ConfigRequest) => {
   const [data, setData] = useState<any>();
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<ErrorAxios | null>({} as ErrorAxios);
+  const [start, setStart] = useState<boolean>(true);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const res = await axiosInstance[method](url, {
+        ...config,
+      });
+
+      setData(res.data);
+    } catch (error: any) {
+      if ('message' in error) {
+        setError({ ...error });
+      } else {
+        console.log('Erro inesperado:', error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [axiosInstance, config, method, url]);
+
+  const sendData = () => {
+    fetchData();
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axiosInstance[method](url, {
-          ...config,
-        });
-
-        setData(res.data);
-      } catch (error: any) {
-        if ('message' in error) {
-          setError({ ...error });
-        } else {
-          console.log('Erro inesperado:', error);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (loading) {
-      fetchData().then((r) => r);
+    if (start) {
+      fetchData();
+      setStart(false);
     }
-  }, [axiosInstance, method, url, config, loading]);
+  }, [start, fetchData]);
 
-  return [data, loading, error];
+  return [data, loading, error, sendData];
 };
 
 export { useAxios };
