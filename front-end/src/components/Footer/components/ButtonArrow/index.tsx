3cb4a5ca1c4156f1496d5c333e2bf90984f0dd45
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 11:50:03 -0300

    feat: delete function scroolToTop and insert import funcions

--- front-end/src/components/Footer/components/ButtonArrow/index.tsx
+++ front-end/src/components/Footer/components/ButtonArrow/index.tsx
@@ -1,13 +1,7 @@
+import { scrollToTop } from '@/functions/scroll';
 import style from './buttonArrow.module.scss';
 
 const ButtonArrow = () => {
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  };
-
   return (
     <button className={style.button} type='button' onClick={() => scrollToTop()}>
       <div />
