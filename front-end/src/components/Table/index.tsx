3b44457930d4bd1e10734e82ed62c4cf92bf3f59
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 12:18:55 -0300

    feat: insert news props and classNames for insert more className and modified for table User and Property

--- front-end/src/components/Table/index.tsx
+++ front-end/src/components/Table/index.tsx
@@ -2,20 +2,24 @@ import { Property, User } from '@/types';
 import InputCheck from '../InputCheck';
 import style from './table.module.scss';
 import { FormEvent } from 'react';
+import classNames from 'classnames';
 
 interface TableProps {
   iten: 'property' | 'user';
-  onChangeRadio: (e: FormEvent<HTMLInputElement>) => void;
+  onChangeRadio?: (e: FormEvent<HTMLInputElement>) => void;
+  idItem: number;
   properties?: Property[];
-  idProperty?: number;
   users?: User[];
+  about?: 'user' | 'adm';
 }
 
 const Table: React.FC<TableProps> = ({
   iten,
   properties,
   onChangeRadio,
-  idProperty,
+  idItem,
+  users,
+  about,
 }: TableProps) => {
   return (
     <table className={style.table}>
@@ -37,15 +41,41 @@ const Table: React.FC<TableProps> = ({
                     type='checkbox'
                     label={`${id}`}
                     value={`${id}`}
-                    className={style.label}
-                    checked={idProperty === id}
+                    disabled={about === 'user' && situation === 'solicitação de exclusão'}
+                    className={classNames({
+                      [style.label]: true,
+                      [style.disabled]: about === 'user' && situation === 'solicitação de exclusão',
+                    })}
+                    checked={idItem === id}
                     onChange={onChangeRadio}
                   />
                 </form>
               </td>
-              <td>{business}</td>
+              <td>{business === 'ambos' ? 'aluguel e venda' : business}</td>
               <td>{`${address} - ${number}`}</td>
               <td>{situation}</td>
+            </tr>
+          ))}
+        {users &&
+          users.map(({ id, name, email, role }) => (
+            <tr key={id}>
+              <td>
+                <form>
+                  <InputCheck
+                    type='checkbox'
+                    label={`${id}`}
+                    value={`${id}`}
+                    className={classNames({
+                      [style.label]: true,
+                    })}
+                    checked={idItem === id}
+                    onChange={onChangeRadio}
+                  />
+                </form>
+              </td>
+              <td>{name}</td>
+              <td>{email}</td>
+              <td>{role === 'client' ? 'Comum' : 'ADM'}</td>
             </tr>
           ))}
       </tbody>
