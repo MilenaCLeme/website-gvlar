70b604468209c4b9fa3254f1dca233fda061ea4f
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 10:26:05 -0300

    feat: functions for: frist word big, transform text for word and remove Letters And Limit

--- front-end/src/functions/text/index.ts
+++ front-end/src/functions/text/index.ts
@@ -1,3 +1,22 @@
-export function firstWord(texto: string) {
-  return texto.split(' ')[0];
+export function firstWord(text: string) {
+  return text.split(' ')[0];
+}
+
+export function transformText(text: string | undefined, about: string) {
+  if (about === 'property') {
+    return text === '' ? 'Imovél' : text;
+  }
+
+  if (about === 'user') {
+    return text === '' ? 'Função' : text === 'master' || text === 'worker' ? 'ADM' : 'Comum';
+  }
+
+  if (about === 'adm') {
+    return text;
+  }
+}
+
+export function removeLettersAndLimit(str: string): string {
+  const numbersOnly = str.replace(/[^\d]/g, ''); // Remove letters, keeping only numbers
+  return numbersOnly.slice(0, 4);
 }
