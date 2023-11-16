1f6c4b52687c9168d5e9baa15d9dbbbf86bd3291
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 11:51:10 -0300

    feat: insert news props to LINk and useContext for user.roler

--- front-end/src/components/Footer/components/NavFooter/index.tsx
+++ front-end/src/components/Footer/components/NavFooter/index.tsx
@@ -1,7 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import style from './navFooter.module.scss';
+import { useContext } from 'react';
+import { Context } from '@/context';
 
 const NavFooter = () => {
+  const { user } = useContext(Context);
   return (
     <>
       <NavLink className={style.home} to='/'>
@@ -14,10 +17,10 @@ const NavFooter = () => {
       <NavLink className={style.filter} to='/'>
         Encontre o seu imóvel
       </NavLink>
-      <NavLink className={style.announce} to='/'>
+      <NavLink className={style.announce} to={user ? '/adm/imoveis' : '/login'}>
         Anuncie o seu imóvel
       </NavLink>
-      <NavLink className={style.login} to='/'>
+      <NavLink className={style.login} to={user ? '/adm' : '/login'}>
         <span>Minha conta</span>
         <div />
       </NavLink>
