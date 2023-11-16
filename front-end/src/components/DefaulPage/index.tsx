c58073c25fc5fc4a9c9bab472dcf7e7af2f1f71b
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 10:38:39 -0300

    feat: insert pathname for insert style different

--- front-end/src/components/DefaulPage/index.tsx
+++ front-end/src/components/DefaulPage/index.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../Header';
 import Footer from '../Footer';
 import style from './defaulPage.module.scss';
+import classNames from 'classnames';
 
 const DefaulPage = () => {
+  const { pathname } = useLocation();
   return (
     <>
       <Header />
-      <main className={style.main}>
+      <main className={classNames({ [style.main]: true, [style.home]: pathname === '/' })}>
         <Outlet />
       </main>
       <Footer />
