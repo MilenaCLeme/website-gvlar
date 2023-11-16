d809e21ce07b1cdb580011c4d1e0538224dc667e
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 15:20:34 -0300

    feat: insert route user for api: updatePartialUser, deleteUser

--- front-end/src/service/api/user/index.ts
+++ front-end/src/service/api/user/index.ts
@@ -1,5 +1,5 @@
 import api from '../axios-config';
-import { ErrorAxios, GetUser, Login } from '@/types';
+import { ErrorAxios, GetUser, Login, User } from '@/types';
 
 export const LoginUser = async (body: Login) => {
   try {
@@ -7,6 +7,55 @@ export const LoginUser = async (body: Login) => {
       ...body,
     });
     return data as GetUser;
+  } catch (error: any) {
+    if ('message' in error) {
+      return error as ErrorAxios;
+    } else {
+      console.log('Erro inesperado:', error);
+    }
+  }
+};
+
+export const updatePartialUser = async (id: number, body: User, token: string) => {
+  try {
+    const update =
+      body.role === 'master'
+        ? {
+            name: body.name,
+            phone: body.phone,
+            email: body.email,
+          }
+        : {
+            name: body.name,
+            role: body.role,
+            phone: body.phone,
+            email: body.email,
+          };
+    const { data } = await api.patch<User>(
+      `/users/${id}`,
+      {
+        ...update,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    );
+    return data as User;
+  } catch (error: any) {
+    if ('message' in error) {
+      return error as ErrorAxios;
+    } else {
+      console.log('Erro inesperado:', error);
+    }
+  }
+};
+
+export const deleteUser = async (id: number, token: string) => {
+  try {
+    const { data } = await api.delete<User>(`/users/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return data as User;
   } catch (error: any) {
     if ('message' in error) {
       return error as ErrorAxios;
