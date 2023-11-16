0781944d122c17f3175f1fb005cad20f74621ab1
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 12:11:17 -0300

    feat: modified component for types different user or adm

--- front-end/src/components/InputSelect/index.tsx
+++ front-end/src/components/InputSelect/index.tsx
@@ -3,6 +3,7 @@ import style from './inputSelect.module.scss';
 import classNames from 'classnames';
 import ArrowBlack from '@/assets/arrow/arrowGvBlack.svg';
 import ArrowOrange from '@/assets/arrow/arrowGvOrange.svg';
+import { transformText } from '@/functions/text';
 
 interface Option {
   value: string;
@@ -10,20 +11,40 @@ interface Option {
 }
 
 interface InputSelectProps {
+  iten: 'property' | 'user' | 'adm';
   valeu?: string;
   handleChange: (e: FormEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
 }
 
-const InputSelect: React.FC<InputSelectProps> = ({ valeu, handleChange }: InputSelectProps) => {
+const InputSelect: React.FC<InputSelectProps> = ({
+  iten,
+  valeu,
+  handleChange,
+  disabled,
+}: InputSelectProps) => {
   const inputRef = useRef<HTMLDivElement>(null);
   const [open, setOPen] = useState<boolean>(false);
 
-  const options: Option[] = [
+  const optionsProperty: Option[] = [
     { value: 'Apartamento', label: 'Apartamento' },
     { value: 'Casa Térreo', label: 'Casa Térreo' },
     { value: 'Sobrado', label: 'Sobrado' },
     { value: 'Galpão', label: 'Galpão' },
     { value: 'Terreno', label: 'Terreno' },
+  ];
+
+  const optionsUser: Option[] = [
+    { value: 'client', label: 'Comum' },
+    { value: 'worker', label: 'ADM' },
+  ];
+
+  const optionsAdm: Option[] = [
+    { value: 'liberado', label: 'liberado' },
+    { value: 'em analise', label: 'em analise' },
+    { value: 'solicitação de exclusão', label: 'solicitação de exclusão' },
+    { value: 'vendeu', label: 'vendeu' },
+    { value: 'alugado', label: 'alugado' },
   ];
 
   useEffect(() => {
@@ -43,18 +64,26 @@ const InputSelect: React.FC<InputSelectProps> = ({ valeu, handleChange }: InputS
   }, [open]);
 
   return (
-    <div className={style.div} ref={inputRef}>
+    <div className={classNames({ [style.div]: true, [style.adm]: iten === 'adm' })} ref={inputRef}>
       <button
         className={classNames({ [style.select]: true, [style.open]: open })}
         name='select'
         type='button'
+        disabled={disabled}
       >
-        {valeu === '' ? 'Imovél' : valeu}
+        {transformText(valeu, iten)}
         {open ? <ArrowOrange /> : <ArrowBlack />}
       </button>
-      <div className={classNames({ [style.options]: open, [style.closet]: !open })}>
+      <div
+        className={classNames({
+          [style.options]: open,
+          [style.closet]: !open,
+          [style.user]: iten === 'user',
+        })}
+      >
         {open &&
-          options.map(({ label }, index) => (
+          iten === 'property' &&
+          optionsProperty.map(({ label }, index) => (
             <button
               onClick={(e: FormEvent<HTMLButtonElement>) => {
                 handleChange(e);
@@ -64,6 +93,38 @@ const InputSelect: React.FC<InputSelectProps> = ({ valeu, handleChange }: InputS
               type='button'
               name='about'
               value={label}
+            >
+              {label}
+            </button>
+          ))}
+        {open &&
+          iten === 'user' &&
+          optionsUser.map(({ label, value }, index) => (
+            <button
+              onClick={(e: FormEvent<HTMLButtonElement>) => {
+                handleChange(e);
+                setOPen(false);
+              }}
+              key={index}
+              type='button'
+              name='role'
+              value={value}
+            >
+              {label}
+            </button>
+          ))}
+        {open &&
+          iten === 'adm' &&
+          optionsAdm.map(({ label, value }, index) => (
+            <button
+              onClick={(e: FormEvent<HTMLButtonElement>) => {
+                handleChange(e);
+                setOPen(false);
+              }}
+              key={index}
+              type='button'
+              name='situation'
+              value={value}
             >
               {label}
             </button>
