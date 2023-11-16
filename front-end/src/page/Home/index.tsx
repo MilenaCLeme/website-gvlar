9140439f6000bab5138e2ec52e88c3ead1cb0837
Milena Leme <milenaleme4@hotmail.com>
2023-11-16 14:40:44 -0300

    feat: component Home for insert Router /

---
+++ front-end/src/page/Home/index.tsx
@@ -0,0 +1,17 @@
+import Announce from './Announce';
+import Banner from './Banner';
+import Company from './Company';
+import Opportunities from './Opportunities';
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Announce />
+      <Company />
+      <Opportunities />
+    </>
+  );
+};
+
+export default Home;
