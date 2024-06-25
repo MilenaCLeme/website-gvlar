c2e991399a2d8b6151e2d38d8fae83fb7b074ad3
Milena Leme <milenaleme4@hotmail.com>
2024-06-25 17:22:58 -0300

    test: mock prisma service unit

--- back-end/src/testing/prisma-repository.mock.ts
+++ back-end/src/testing/prisma-repository.mock.ts
@@ -1,5 +1,6 @@
 import { PrismaService } from '../prisma/prisma.service';
 import { userEntityList } from './user-entity-list.mock';
+import { commentEntityList } from './comment/comment-entity-list.mock';
 
 export const PrismaRepositoryMock = {
   provide: PrismaService,
@@ -12,6 +13,14 @@ export const PrismaRepositoryMock = {
       delete: jest.fn().mockResolvedValue(userEntityList[3]),
       count: jest.fn().mockResolvedValue(1),
       findFirst: jest.fn().mockResolvedValue(userEntityList[0]),
+    },
+    comment: {
+      create: jest.fn().mockResolvedValue(commentEntityList[2]),
+      findMany: jest.fn().mockResolvedValue(commentEntityList),
+      findUnique: jest.fn().mockResolvedValue(commentEntityList[0]),
+      update: jest.fn().mockResolvedValue(commentEntityList[2]),
+      delete: jest.fn().mockResolvedValue(commentEntityList[2]),
+      count: jest.fn().mockResolvedValue(1),
     },
   },
 };
