44ae6c3009766adc3b8cef7ee0db8098392cbe69
Milena Leme <milenaleme4@hotmail.com>
2024-06-25 17:23:32 -0300

    test: mock property service unit

--- back-end/src/testing/propertyService.mock.ts
+++
@@ -1,9 +0,0 @@
-import { PropertyService } from '../property/property.service';
-
-export const propertyRepositoryMock = {
-  provide: PropertyService,
-  useValue: {
-    updateRegisterForUserMaster: jest.fn(),
-    sendEmailConfirmtion: jest.fn().mockResolvedValue(true),
-  },
-};
